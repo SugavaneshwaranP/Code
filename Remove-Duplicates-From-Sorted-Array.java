cf38b39fef1b8011442cc9a8346e39e2bc169bf3
SugavaneshwaranP <sugavanesh08@gmail.com>
2025-07-12 09:38:12 +0530

    code

---
+++ Remove-Duplicates-From-Sorted-Array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k = 1;
+        int prev = nums[0];
+        for(int i = 1;i<nums.length;i++)
+        {
+            if(nums[i]==prev)
+            {
+                continue;
+            }
+            else
+            {
+                nums[k] = nums[i];
+                prev = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
