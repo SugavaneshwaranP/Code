d4064b58fbebc1ce7bd13d5bcab0a0761c8d0404
SugavaneshwaranP <sugavanesh08@gmail.com>
2025-08-04 21:36:16 +0530

    fix: correct variable name casing for maxOr to maxor in countMaxOrSubsets method

--- count-number-of-maximum-bitwise-or-subsets.java
+++ count-number-of-maximum-bitwise-or-subsets.java
@@ -2,15 +2,15 @@ class Solution {
     public int countMaxOrSubsets(int[] nums) {
         int[] dp = new int[1 << 17];
         dp[0] = 1;
-        int maxOr = 0;
+        int maxor = 0;
 
         for (int num : nums) {
             for (int i = maxOr; i >= 0; i--) {
                 dp[i | num] += dp[i];
             }
-            maxOr |= num;
+            maxor |= num;
         }
 
-        return dp[maxOr];
+        return dp[maxor];
     }
 }
